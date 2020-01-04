ed956fbdeb1318a87ebdbb1584659fcf5c0a5e2b
Nathaniel Zeeman <nathanielzeeman@gmail.com>
2020-01-03 18:00:03 -0800

    created sample 'game'

---
+++ Assets/Scripts/LocalPlayerBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalPlayerBehaviour : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        Vector2 pos = transform.position;
+        pos += new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * Time.deltaTime * speed;
+        transform.position = pos;
+    }
+}
